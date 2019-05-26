0a6697a6f33a587f8b837c2c7c275ddccba0c54e
Anne Buit <anne.buit@gmail.com>
2019-05-26 21:43:05 +0200

    Word detection

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,18 @@ fn main() {
     println!("Data {}" input_str);
     */
 
+    let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+    create_word_index(lorem);
+
     //Build a vector with words, each word a weight - a chance to be chosen
     //This will be the occurrence of this word after the previous word in the provided text.
     let mut words_weighted = Vec::new();
-    words_weighted.push(("Lorem", 5));
-    words_weighted.push(("Ipsum", 5));
-    words_weighted.push(("Dolor", 5));
-    words_weighted.push(("Sit", 5));
-    words_weighted.push(("Amet", 14));
+    words_weighted.push((&lorem[0..5], 5));
+    words_weighted.push((&lorem[6..11], 5));
+    words_weighted.push((&lorem[12..17], 5));
+    words_weighted.push((&lorem[18..21], 5));
+    words_weighted.push((&lorem[22..26], 5));
 
     //Use the weight to create a distribution so we can run a random between 1 and highest and pick a value.
     let ref_words_weighted = &mut words_weighted;
@@ -42,6 +46,43 @@ fn main() {
     println!("Press enter..");
     stdin().read(&mut [0]).unwrap();
     */
+}
+
+fn create_word_index (text: &str) -> Vec<(&str, Vec<(&str, i32)>)>
+{
+    let mut counter = 0;
+    let mut word_index: Vec<(&str, Vec<(&str, i32)>)> = Vec::new();
+    let mut word_start = 0;
+    while counter < text.len()
+    {
+        if &text[counter..counter + 1] == " "
+        {
+            println!("word: {} ", &text[word_start..counter]);
+
+            if !word_index.iter().any(|known_word: &(&str, std::vec::Vec<(&str, i32)>)| known_word.0 == &text[word_start..counter])
+            {
+                println!("word {} is new!", &text[word_start..counter]);
+                word_index.push((&text[word_start..counter], Vec::new()));
+            }
+
+            //Start the next word
+            word_start = counter + 1;
+        }
+        else if counter + 1 == text.len()
+        {
+            println!("last word: {} ", &text[word_start..counter + 1]);
+
+            if !word_index.iter().any(|known_word: &(&str, std::vec::Vec<(&str, i32)>)| known_word.0 == &text[word_start..counter + 1])
+            {
+                println!("word {} is new!", &text[word_start..counter + 1]);
+                word_index.push((&text[word_start..counter + 1], Vec::new()));
+            }
+        }
+
+        counter += 1;
+    }
+
+    word_index
 }
 
 fn modify_to_distributed_range(words_weighted: &mut Vec<(&str, i32)>)
